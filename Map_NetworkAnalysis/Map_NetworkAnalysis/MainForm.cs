1477ece7ad1ffccd6ce6b0a73afb4e9c5c084400
T-urning <15797682325@163.com>
2018-06-11 13:35:06 +0800

    first commit

---
+++ Map_NetworkAnalysis/Map_NetworkAnalysis/MainForm.cs
@@ -0,0 +1,203 @@
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.ADF;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.NetworkAnalyst;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.DataSourcesGDB;
+
+namespace Map_NetworkAnalysis
+{
+    public sealed partial class MainForm : Form
+    {
+        #region class private members
+        private IMapControl3 m_mapControl = null;
+        private string m_mapDocumentName = string.Empty;
+        #endregion
+        private string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+
+        #region class constructor
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //get the MapControl
+            m_mapControl = (IMapControl3)axMapControl1.Object;
+
+            //disable the Save menu (since there is no document yet)
+            menuSaveDoc.Enabled = false;
+        }
+
+        #region Main Menu event handlers
+        private void menuNewDoc_Click(object sender, EventArgs e)
+        {
+            //execute New Document command
+            ICommand command = new CreateNewDocument();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuOpenDoc_Click(object sender, EventArgs e)
+        {
+            //execute Open Document command
+            ICommand command = new ControlsOpenDocCommandClass();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuSaveDoc_Click(object sender, EventArgs e)
+        {
+            //execute Save Document command
+            if (m_mapControl.CheckMxFile(m_mapDocumentName))
+            {
+                //create a new instance of a MapDocument
+                IMapDocument mapDoc = new MapDocumentClass();
+                mapDoc.Open(m_mapDocumentName, string.Empty);
+
+                //Make sure that the MapDocument is not readonly
+                if (mapDoc.get_IsReadOnly(m_mapDocumentName))
+                {
+                    MessageBox.Show("Map document is read only!");
+                    mapDoc.Close();
+                    return;
+                }
+
+                //Replace its contents with the current map
+                mapDoc.ReplaceContents((IMxdContents)m_mapControl.Map);
+
+                //save the MapDocument in order to persist it
+                mapDoc.Save(mapDoc.UsesRelativePaths, false);
+
+                //close the MapDocument
+                mapDoc.Close();
+            }
+        }
+
+        private void menuSaveAs_Click(object sender, EventArgs e)
+        {
+            //execute SaveAs Document command
+            ICommand command = new ControlsSaveAsDocCommandClass();
+            command.OnCreate(m_mapControl.Object);
+            command.OnClick();
+        }
+
+        private void menuExitApp_Click(object sender, EventArgs e)
+        {
+            //exit the application
+            Application.Exit();
+        }
+        #endregion
+
+        //listen to MapReplaced evant in order to update the statusbar and the Save menu
+        private void axMapControl1_OnMapReplaced(object sender, IMapControlEvents2_OnMapReplacedEvent e)
+        {
+            //get the current document name from the MapControl
+            m_mapDocumentName = m_mapControl.DocumentFilename;
+
+            //if there is no MapDocument, diable the Save menu and clear the statusbar
+            if (m_mapDocumentName == string.Empty)
+            {
+                menuSaveDoc.Enabled = false;
+                statusBarXY.Text = string.Empty;
+            }
+            else
+            {
+                //enable the Save manu and write the doc name to the statusbar
+                menuSaveDoc.Enabled = true;
+                statusBarXY.Text = System.IO.Path.GetFileName(m_mapDocumentName);
+            }
+        }
+
+        private void axMapControl1_OnMouseMove(object sender, IMapControlEvents2_OnMouseMoveEvent e)
+        {
+            statusBarXY.Text = string.Format("{0}, {1}  {2}", e.mapX.ToString("#######.##"), e.mapY.ToString("#######.##"), axMapControl1.MapUnits.ToString().Substring(4));
+        }
+
+        private void axTOCControl1_OnMouseDown(object sender, ITOCControlEvents_OnMouseDownEvent e)
+        {
+
+        }
+        //加载站点
+        private void addStopsMenuItem_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new AddNetStopsTool();
+            pCommand.OnCreate(axMapControl1.Object);
+            axMapControl1.CurrentTool = pCommand as ITool;
+            pCommand = null;
+        }
+        //加载障碍点
+        private void addBarriesMenuItem_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new AddNetBarriesTool();
+            pCommand.OnCreate(axMapControl1.Object);
+            axMapControl1.CurrentTool = pCommand as ITool;
+            pCommand = null;
+        }
+        //最短路径分析
+        private void routeSolverMenuItem_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new ShortPathSolveCommand();
+            pCommand.OnCreate(axMapControl1.Object);
+            pCommand.OnClick();
+            pCommand = null;
+        }
+        private void clearResultMenuItem_Click(object sender, EventArgs e)
+        {
+            axMapControl1.CurrentTool = null;
+            try
+            {
+                string name = NetWorkAnalysClass.getPath(path) + "\\data\\HuanbaoGeodatabase.gdb";
+                //打开工作空间
+                IFeatureWorkspace pFWorkspace = NetWorkAnalysClass.OpenWorkspace(name) as IFeatureWorkspace;
+                IGraphicsContainer pGrap = this.axMapControl1.ActiveView as IGraphicsContainer;
+                pGrap.DeleteAllElements();//删除所添加的图片要素
+                IFeatureClass inputFClass = pFWorkspace.OpenFeatureClass("Stops");//站点要素
+                IFeatureClass barriesFClass = pFWorkspace.OpenFeatureClass("barries");//障碍点要素
+                if (inputFClass.FeatureCount(null) > 0)
+                {
+                    ITable pTable = barriesFClass as ITable;
+                    pTable.DeleteSearchedRows(null);
+                }
+                for (int i = 0; i < axMapControl1.LayerCount; i++) //删除分析结果
+                {
+                    ILayer pLayer = axMapControl1.get_Layer(i);
+                    if (pLayer.Name == ShortPathSolveCommand.m_NAContext.Solver.DisplayName)
+                    {
+                        axMapControl1.DeleteLayer(i);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            this.axMapControl1.Refresh();
+        }
+
+        private void axMapControl1_OnMouseDown(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Map_NetworkAnalysis/Map_NetworkAnalysis/ShortPathSolveCommand.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.NetworkAnalyst;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Display;
+using System.Windows.Forms;
+using ESRI.ArcGIS.esriSystem;
+
+namespace Map_NetworkAnalysis
+{
+    /// <summary>
+    /// Command that works in ArcMap/Map/PageLayout
+    /// </summary>
+    [Guid("431b845b-b502-422f-82e5-84f8dd7da09f")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("Map_NetworkAnalysis.ShortPathSolveCommand")]
+    public sealed class ShortPathSolveCommand : BaseCommand
+    {
+        #region COM Registration Function(s)
+        [ComRegisterFunction()]
+        [ComVisible(false)]
+        static void RegisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryRegistration(registerType);
+
+            //
+            // TODO: Add any COM registration code here
+            //
+        }
+
+        [ComUnregisterFunction()]
+        [ComVisible(false)]
+        static void UnregisterFunction(Type registerType)
+        {
+            // Required for ArcGIS Component Category Registrar support
+            ArcGISCategoryUnregistration(registerType);
+
+            //
+            // TODO: Add any COM unregistration code here
+            //
+        }
+
+        #region ArcGIS Component Category Registrar generated code
+        /// <summary>
+        /// Required method for ArcGIS Component Category registration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryRegistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Register(regKey);
+            ControlsCommands.Register(regKey);
+        }
+        /// <summary>
+        /// Required method for ArcGIS Component Category unregistration -
+        /// Do not modify the contents of this method with the code editor.
+        /// </summary>
+        private static void ArcGISCategoryUnregistration(Type registerType)
+        {
+            string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+            MxCommands.Unregister(regKey);
+            ControlsCommands.Unregister(regKey);
+        }
+
+        #endregion
+        #endregion
+
+        private IHookHelper m_hookHelper = null;
+
+        public static INAContext m_NAContext;
+        private INetworkDataset networkDataset;
+        private IFeatureClass inputFClass;
+        private IFeatureClass barriesFClass;
+        string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+        public ShortPathSolveCommand()
+        {
+            //
+            // TODO: Define values for the public properties
+            //
+            base.m_category = "NetworkAnalyst"; //localizable text
+            base.m_caption = "路径解决";  //localizable text 
+            base.m_message = "得到最短路径";  //localizable text
+            base.m_toolTip = "路径解决";  //localizable text
+            base.m_name = "ShortPathSolver";   //unique id, non-localizable (e.g. "MyCategory_MyCommand")
+
+            try
+            {
+                //
+                // TODO: change bitmap name if necessary
+                //
+                string bitmapResourceName = GetType().Name + ".bmp";
+                base.m_bitmap = new Bitmap(GetType(), bitmapResourceName);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message, "Invalid Bitmap");
+            }
+        }
+
+        #region Overridden Class Methods
+
+        /// <summary>
+        /// Occurs when this command is created
+        /// </summary>
+        /// <param name="hook">Instance of the application</param>
+        public override void OnCreate(object hook)
+        {
+            if (hook == null)
+                return;
+
+            try
+            {
+                m_hookHelper = new HookHelperClass();
+                m_hookHelper.Hook = hook;
+                if (m_hookHelper.ActiveView == null)
+                    m_hookHelper = null;
+            }
+            catch
+            {
+                m_hookHelper = null;
+            }
+
+            if (m_hookHelper == null)
+                base.m_enabled = false;
+            else
+                base.m_enabled = true;
+
+            // TODO:  Add other initialization code
+        }
+
+        /// <summary>
+        /// Occurs when this command is clicked
+        /// </summary>
+        public override void OnClick()
+        {
+            string name = NetWorkAnalysClass.getPath(path) + "\\data\\HuanbaoGeodatabase.gdb";//得到数据库文件路径
+            IFeatureWorkspace pFWorkspace = NetWorkAnalysClass.OpenWorkspace(name) as IFeatureWorkspace;//打开要素工作空间
+            //"RouteNetwork","BaseData"参数不可更改
+            networkDataset = NetWorkAnalysClass.OpenPathNetworkDataset(pFWorkspace as IWorkspace,
+                "RouteNetwork","BaseData");//打开网络数据集
+            m_NAContext = NetWorkAnalysClass.CreatePathSolverContext(networkDataset);//通过网络数据集创建网络分析上下文
+
+
+            inputFClass = pFWorkspace.OpenFeatureClass("Stops");  //根据名字打开站点要素类
+            barriesFClass = pFWorkspace.OpenFeatureClass("Barries");  //根据名字打开障碍点要素类
+            if (IFLayerExist("NetworkDataset") == false)//若不存在NetworkDataset图层就执行
+            {
+                ILayer layer;
+                INetworkLayer networkLayer;
+                networkLayer = new NetworkLayerClass();
+                networkLayer.NetworkDataset = networkDataset;
+                layer = networkLayer as ILayer;
+                layer.Name = "NetworkDataset";
+                m_hookHelper.ActiveView.FocusMap.AddLayer(layer);
+                layer.Visible = false;
+            }
+            //检查是否存在名为“Route”的图层。若存在则删除
+            if (IFLayerExist(m_NAContext.Solver.DisplayName) == true)
+            {
+                for (int i = 0; i < m_hookHelper.FocusMap.LayerCount; i++)
+                {
+                    if (m_hookHelper.FocusMap.get_Layer(i).Name == m_NAContext.Solver.DisplayName)
+                    {
+                        m_hookHelper.FocusMap.DeleteLayer(m_hookHelper.FocusMap.get_Layer(i));
+                    }
+                }
+            }
+            INALayer naLayer = m_NAContext.Solver.CreateLayer(m_NAContext);
+            ILayer pLayer = naLayer as ILayer;
+            pLayer.Name = m_NAContext.Solver.DisplayName;
+            m_hookHelper.ActiveView.FocusMap.AddLayer(pLayer);//往当前地图中添加该图层
+            
+
+            int featureCount = inputFClass.FeatureCount(null);//得到站点要素类中要素的个数
+            if (featureCount < 2)//若少于两个则不能进行分析
+            {
+                MessageBox.Show("只有一个站点，不能进行路径分析！");
+                return;
+            }
+            IGPMessages gpMessages = new GPMessagesClass();//定义一个地理处理返回信息对象
+            //加载站点要素，并设置容差
+            NetWorkAnalysClass.LoadNANetworkLocations("Stops", inputFClass, m_NAContext, 80);
+            //加载障碍点要素，并设置容差
+            NetWorkAnalysClass.LoadNANetworkLocations("Barriers",barriesFClass, m_NAContext, 20);
+            INASolver naSolver = m_NAContext.Solver;//创建网络分析对象
+            try
+            {
+                naSolver.Solve(m_NAContext, gpMessages, null);//执行最短路径分析
+                
+
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("未能找到有效路径"+ex.Message,"提示",
+                    MessageBoxButtons.OKCancel,MessageBoxIcon.Warning);
+                return;
+            }
+            //将“Routes”图层组下的“Stops”、“Point Barriers”图层设为不可见
+            for (int i = 0; i < m_hookHelper.FocusMap.LayerCount; i++)
+            {
+                if (m_hookHelper.FocusMap.get_Layer(i).Name == m_NAContext.Solver.DisplayName)
+                {
+                    //ICompositeLayer Interface Provides access to members that work with a collection of layers that behaves like a single layer.
+                    ICompositeLayer pCompositeLayer = m_hookHelper.FocusMap.get_Layer(i) as ICompositeLayer;
+                    for (int t = 0; t < pCompositeLayer.Count; t++)
+                    {
+                        ILayer pResultLayer = pCompositeLayer.get_Layer(t);
+                        if (pResultLayer.Name == "Stops" || pResultLayer.Name == "Point Barriers")
+                        {
+                            pResultLayer.Visible = false;
+                            continue;
+                        }
+                    }
+                }
+            }
+           
+
+            IGeoDataset geoDataset;//地理数据集
+            IEnvelope envelope;
+            geoDataset = m_NAContext.NAClasses.get_ItemByName("Routes") as IGeoDataset;
+            //The IGeoDataset::Extent property returns an envelope representing the maximum extent of data which has been stored in the dataset.
+            envelope = geoDataset.Extent;
+            if (!envelope.IsEmpty)
+            {
+                envelope.Expand(1.1, 1.1, true);
+            }
+            //将地图的显示的范围设置为“Routes”图层的数据范围
+            m_hookHelper.ActiveView.Extent = envelope;
+            //刷新视图
+            m_hookHelper.ActiveView.Refresh();
+
+
+            #region 显示路径详细信息
+            //GetResults(naLayer);
+            ITable table = m_NAContext.NAClasses.get_ItemByName("Routes") as ITable;
+            //int count = table.RowCount(null);
+            ICursor cursor = table.Search(null,false);
+            IRow row = cursor.NextRow();
+           
+            //int wor = row.Fields.FieldCount;
+            for (int i = 0; i < table.Fields.FieldCount; i++)
+            {
+                if (table.Fields.get_Field(i).AliasName == "Total_Shape_Length")
+                {
+                    MessageBox.Show("路径长度："+ row.get_Value(i) + "米","路径信息",
+                        MessageBoxButtons.OK,MessageBoxIcon.None);
+                }      
+            }
+                
+            //IFeatureClass routesData = geoDataset as IFeatureClass;
+            //IFeatureClass routes = m_NAContext.NAClasses.get_ItemByName("Routes") as IFeatureClass;
+            //int counti = routes.FeatureCount(null);
+            //IFeatureCursor featureCursor = routesData.Search(null, false);
+            //IFeature shortPath = featureCursor.NextFeature();
+            
+            //string fieldName;
+            //string aliaName;
+            //for (int i = 0; i < feature.Fields.FieldCount; i++)
+            //{
+            //    fieldName = feature.Fields.get_Field(i).Name;
+            //    aliaName = feature.Fields.get_Field(i).AliasName;
+            //}
+
+            #endregion
+           
+        }
+
+        #endregion
+        private bool IFLayerExist(string layerName)
+        {
+            for (int i = 0; i < m_hookHelper.FocusMap.LayerCount; i++)
+            {
+                if (m_hookHelper.FocusMap.get_Layer(i).Name == layerName)
+                    return true;
+
+            }
+            return false;
+        }
+        public void GetResults(INALayer naLayer)
+        {
+            IPropertySet propertySet = naLayer.Context.Result.OutputProperties;
+            object names;
+            object values;
+            propertySet.GetAllProperties(out names, out values);
+            for (int i = 0; i < propertySet.Count; i++)
+            {
+                Console.WriteLine("{0} = {1}", ((string[])names)[i], ((object[])values)[i]);
+            }
+        }
+    }
+
+
+    
+}
+
